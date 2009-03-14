ff2558135c3bc75028be21f8f35dbc1dee34d24f
anonbeat <anonbeat@4cb5adbf-918c-4dcd-961d-2ea90eaf881c>
2009-03-14 23:19:25 +0000

    added menu entry and icon.

--- po/es/CMakeFiles/translations.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/translations"
-  "guayadeque.mo"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/translations.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
