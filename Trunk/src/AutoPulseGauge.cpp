b38900e4ed074c26f2230cf71d2906733211c838
anonbeat <anonbeat@4cb5adbf-918c-4dcd-961d-2ea90eaf881c>
2009-03-26 18:43:12 +0000

    Added AutoPulseGauge

---
+++ Trunk/src/AutoPulseGauge.cpp
@@ -0,0 +1,57 @@
+// -------------------------------------------------------------------------------- //
+//	Copyright (C) 2008-2009 J.Rios
+//	anonbeat@gmail.com
+//
+//    This Program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2, or (at your option)
+//    any later version.
+//
+//    This Program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; see the file LICENSE.  If not, write to
+//    the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+//    http://www.gnu.org/copyleft/gpl.html
+//
+// -------------------------------------------------------------------------------- //
+#include "AutoPulseGauge.h"
+
+// -------------------------------------------------------------------------------- //
+guAutoPulseGauge::guAutoPulseGauge( wxWindow * parent, wxWindowID id, int range, const wxPoint &pos,
+             const wxSize &size, long style, const wxValidator& validator, const wxString &name ) :
+    wxGauge( parent, id, range, pos, size, style, validator, name )
+{
+    m_Timer = new guGaugeTimer( this );
+    m_Timer->Start( 400 );
+}
+
+// -------------------------------------------------------------------------------- //
+guAutoPulseGauge::~guAutoPulseGauge( void )
+{
+    if( m_Timer )
+        delete m_Timer;
+}
+
+// -------------------------------------------------------------------------------- //
+void guAutoPulseGauge::StopPulse( int range, int value )
+{
+    m_Timer->Stop();
+    SetRange( range );
+    SetValue( value );
+}
+
+// -------------------------------------------------------------------------------- //
+void guAutoPulseGauge::StartPulse( void )
+{
+    m_Timer->Start( 400 );
+}
+
+// -------------------------------------------------------------------------------- //
+bool guAutoPulseGauge::IsPulsing( void )
+{
+    return m_Timer->IsRunning();
+};
