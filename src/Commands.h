7b159ed84c723f687d1daea676a9dab51f9fb0b4
anonbeat <anonbeat@4cb5adbf-918c-4dcd-961d-2ea90eaf881c>
2009-05-12 09:49:15 +0000

    Version changed to v0.0.3 Beta

--- src/Commands.h
+++ src/Commands.h
@@ -21,7 +21,7 @@
 #ifndef COMMANDS_H
 #define COMMANDS_H
 
-#define ID_GUAYADEQUE_VERSION           "v0.0.2 (Beta)"
+#define ID_GUAYADEQUE_VERSION           "v0.0.3 (Beta)"
 
 // -------------------------------------------------------------------------------- //
 //
