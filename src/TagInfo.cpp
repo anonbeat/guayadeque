44f5017232a3b80fc2de068a5c9618ded4b26d6e
anonbeat <anonbeat@4cb5adbf-918c-4dcd-961d-2ea90eaf881c>
2009-05-13 22:26:32 +0000

    Adding support to write tags ID3v2.4

--- src/TagInfo.cpp
+++ src/TagInfo.cpp
@@ -23,14 +23,21 @@
 
 #include <tag.h>
 #include <fileref.h>
+#include <textidentificationframe.h>
+#include <id3v2framefactory.h>
+#include <id3v2tag.h>
+#include <mpegfile.h>
+
 #include <wx/tokenzr.h>
+
+using namespace TagLib;
 
 // -------------------------------------------------------------------------------- //
 bool TagInfo::ReadID3Tags( const wxString &FileName )
 {
-    TagLib::FileRef fileref = TagLib::FileRef( FileName.ToUTF8(), true, TagLib::AudioProperties::Fast );
-    TagLib::Tag * tag;
-    TagLib::AudioProperties * apro;
+    FileRef fileref = TagLib::FileRef( FileName.ToUTF8(), true, TagLib::AudioProperties::Fast );
+    Tag * tag;
+    AudioProperties * apro;
 
     if( !fileref.isNull() )
     {
@@ -42,6 +49,7 @@ bool TagInfo::ReadID3Tags( const wxString &FileName )
             m_GenreName = wxString( tag->genre().toCString( true ), wxConvUTF8 );
             m_Track = tag->track();
             m_Year = tag->year();
+
         }
 
         apro = fileref.audioProperties();
@@ -53,6 +61,168 @@ bool TagInfo::ReadID3Tags( const wxString &FileName )
         {
             guLogWarning( wxT( "Cant read audio properties from %s\n" ), FileName.c_str() );
         }
+
+
+        // If its a ID3v2 Tag try to load the labels
+        ID3v2::Tag * tagv2 = ( ( TagLib::MPEG::File * ) fileref.file() )->ID3v2Tag();
+        if( tagv2 )
+        {
+            if( m_TrackLabels.Count() == 0 )
+            {
+                ID3v2::UserTextIdentificationFrame * Frame = ID3v2::UserTextIdentificationFrame::find( tagv2, "guTRLABELS" );
+                if( Frame )
+                {
+                    m_TrackLabelsStr = wxString( Frame->toString().toCString( true ), wxConvUTF8 );
+                    guLogMessage( wxT( "Track Label: '%s'\n" ), m_TrackLabelsStr.c_str() );
+                    m_TrackLabels = wxStringTokenize( m_TrackLabelsStr, wxT( "|" ) );
+                }
+            }
+
+            if( m_ArtistLabels.Count() == 0 )
+            {
+                ID3v2::UserTextIdentificationFrame * Frame = ID3v2::UserTextIdentificationFrame::find( tagv2, "guARLABELS" );
+                if( Frame )
+                {
+                    m_ArtistLabelsStr = wxString( Frame->toString().toCString( true ), wxConvUTF8 );
+                    guLogMessage( wxT( "Artist Label: '%s'\n" ), m_ArtistLabelsStr.c_str() );
+                    m_ArtistLabels = wxStringTokenize( m_TrackLabelsStr, wxT( "|" ) );
+                }
+            }
+
+            if( m_AlbumLabels.Count() == 0 )
+            {
+                ID3v2::UserTextIdentificationFrame * Frame = ID3v2::UserTextIdentificationFrame::find( tagv2, "guALLABELS" );
+                if( Frame )
+                {
+                    m_AlbumLabelsStr = wxString( Frame->toString().toCString( true ), wxConvUTF8 );
+                    guLogMessage( wxT( "Album Label: '%s'\n" ), m_AlbumLabelsStr.c_str() );
+                    m_AlbumLabels = wxStringTokenize( m_TrackLabelsStr, wxT( "|" ) );
+                }
+            }
+        }
+    }
+    else
+    {
+      guLogError( wxT( "Could not read tags from file %s\n" ), FileName.c_str() );
+    }
+
+    return true;
+}
+
+
+// -------------------------------------------------------------------------------- //
+void ID3v2_CheckLabelFrame( ID3v2::Tag * tagv2, const char * description, const wxString &value )
+{
+    ID3v2::UserTextIdentificationFrame * frame;
+    frame = ID3v2::UserTextIdentificationFrame::find( tagv2, description );
+    if( frame )
+    {
+        if( value.IsEmpty() )
+        {
+            while( frame )
+            {
+                // remove the frame
+                tagv2->removeFrame( frame );
+                frame = ID3v2::UserTextIdentificationFrame::find( tagv2, description );
+            }
+        }
+        else
+        {
+            frame->setText( ( char * ) value.char_str( wxConvISO8859_1 ) );
+        }
+    }
+    else
+    {
+        if( !value.IsEmpty() )
+        {
+            frame = new ID3v2::UserTextIdentificationFrame( TagLib::String::UTF8 );
+            frame->setDescription( TagLib::String( description, TagLib::String::UTF8 ) );
+            frame->setText( ( char * ) value.char_str( wxConvUTF8 ) );
+            tagv2->addFrame( frame );
+        }
+    }
+}
+
+// -------------------------------------------------------------------------------- //
+bool TagInfo::WriteID3Tags( const wxString &FileName )
+{
+    FileRef fileref = TagLib::FileRef( FileName.ToUTF8() );
+    Tag * tag;
+
+    if( !fileref.isNull() )
+    {
+        if( ( tag = fileref.tag() ) )
+        {
+            tag->setTitle( ( char * ) m_TrackName.char_str( wxConvISO8859_1 ) );
+            tag->setArtist( ( char * ) m_ArtistName.char_str( wxConvISO8859_1 ) );
+            tag->setAlbum( ( char * ) m_AlbumName.char_str( wxConvISO8859_1 ) );
+            tag->setGenre( ( char * ) m_GenreName.char_str( wxConvISO8859_1 ) );
+            tag->setTrack( m_Track );
+            tag->setYear( m_Year );
+        }
+
+        // Check if we have a id3v2 Tag
+        ID3v2::Tag * tagv2 = ( ( TagLib::MPEG::File * ) fileref.file() )->ID3v2Tag();
+        if( tagv2 )
+        {
+
+            // The Labels
+            if( m_ArtistLabelsStr.IsEmpty() )
+            {
+                if( m_ArtistLabels.Count() )
+                {
+                    int index;
+                    int count = m_ArtistLabels.Count();
+                    for( index = 0; index < count; index++ )
+                    {
+                        m_ArtistLabelsStr += m_ArtistLabels[ index ];
+                        m_ArtistLabelsStr += wxT( "|" );
+                    }
+                    m_ArtistLabelsStr.RemoveLast( 1 );
+                }
+            }
+
+
+            if( m_AlbumLabelsStr.IsEmpty() )
+            {
+                if( m_AlbumLabels.Count() )
+                {
+                    int index;
+                    int count = m_AlbumLabels.Count();
+                    for( index = 0; index < count; index++ )
+                    {
+                        m_AlbumLabelsStr += m_AlbumLabels[ index ];
+                        m_AlbumLabelsStr += wxT( "|" );
+                    }
+                    m_AlbumLabelsStr.RemoveLast( 1 );
+                }
+            }
+
+
+            if( m_TrackLabelsStr.IsEmpty() )
+            {
+                if( m_TrackLabels.Count() )
+                {
+                    int index;
+                    int count = m_TrackLabels.Count();
+                    for( index = 0; index < count; index++ )
+                    {
+                        m_TrackLabelsStr += m_TrackLabels[ index ];
+                        m_TrackLabelsStr += wxT( "|" );
+                    }
+                    m_TrackLabelsStr.RemoveLast( 1 );
+                }
+            }
+
+            ID3v2_CheckLabelFrame( tagv2, "guARLABELS", m_ArtistLabelsStr );
+            ID3v2_CheckLabelFrame( tagv2, "guALLABELS", m_AlbumLabelsStr );
+            ID3v2_CheckLabelFrame( tagv2, "guTRLABELS", m_TrackLabelsStr );
+
+        }
+
+        if( !fileref.save() )
+          guLogWarning( _( "iD3Tags Save failed" ) );
+
     }
     else
     {
@@ -60,238 +230,7 @@ bool TagInfo::ReadID3Tags( const wxString &FileName )
     }
 
 
-//////  char * pStr;
-//////  if( m_TrackName.IsEmpty() )
-//////  {
-//////    pStr = ID3_GetTitle( tag );
-//////    m_TrackName = wxString( pStr, wxConvISO8859_1 );
-//////    ID3_FreeString( pStr );
-//////  }
-//////
-//////  if( m_ArtistName.IsEmpty() )
-//////  {
-//////    pStr = ID3_GetArtist( tag );
-//////    m_ArtistName = wxString( pStr, wxConvISO8859_1 );
-//////    ID3_FreeString( pStr );
-//////  }
-//////
-//////  if( m_AlbumName.IsEmpty() )
-//////  {
-//////    pStr = ID3_GetAlbum( tag );
-//////    m_AlbumName = wxString( pStr, wxConvISO8859_1 );
-//////    ID3_FreeString( pStr );
-//////  }
-//////
-//////  if( m_Track == 0 )
-//////  {
-//////    m_Track = ID3_GetTrackNum( tag );
-//////  }
-//////
-//////  if( m_Year == 0 )
-//////  {
-//////    pStr = ID3_GetYear( tag );
-//////    wxString( pStr, wxConvISO8859_1 ).ToLong( ( long int * ) &m_Year );
-//////    ID3_FreeString( pStr );
-//////  }
-//////
-//////  if( m_GenreName.IsEmpty() )
-//////  {
-//////    pStr = ID3_GetGenre( tag );
-//////    //printf( "GenreTag : %s\n", pStr );
-//////    m_GenreName = wxString( pStr, wxConvISO8859_1 );
-////////    if( GenreName.IsEmpty() )
-////////      printf( "Genre Error '%s'\n", pStr );
-//////    ID3_FreeString( pStr );
-//////    if( m_GenreName[ 0 ] == '(' )
-//////    {
-//////      int GenreIndex;
-//////      m_GenreName.AfterFirst( '(' ).BeforeFirst( ')' ).ToLong( ( long int * ) &GenreIndex );
-//////      m_GenreName = wxString( ID3_V1GENRE2DESCRIPTION( GenreIndex ), wxConvISO8859_1 );
-//////      //guLogMessage( wxT( "Genre is a index ID3v1 '%s'" ), GenreName.c_str() );
-//////    }
-//////  }
-//////
-//////  if( m_Length == 0 )
-//////  {
-//////    const Mp3_Headerinfo * mp3info;
-//////    mp3info = tag->GetMp3HeaderInfo();
-//////    if( mp3info )
-//////    {
-//////      m_Length = mp3info->time;
-//////    }
-//////  }
-//////
-//////  if( m_TrackLabels.Count() == 0 )
-//////  {
-//////    ID3_Frame * Frame = tag->Find( ID3FID_USERTEXT, ID3FN_DESCRIPTION, "guTRLABELS" );
-//////    if( Frame )
-//////    {
-//////      char Buffer[ 1024 ];
-//////      Frame->Field( ID3FN_TEXT ).Get( Buffer, sizeof( Buffer ) );
-//////      m_TrackLabelsStr = wxString( Buffer, wxConvUTF8 );
-//////      m_TrackLabels = wxStringTokenize( m_TrackLabelsStr, wxT( "|" ) );
-//////    }
-//////  }
-//////
-//////  if( m_ArtistLabels.Count() == 0 )
-//////  {
-//////    ID3_Frame * Frame = tag->Find( ID3FID_USERTEXT, ID3FN_DESCRIPTION, "guARLABELS" );
-//////    if( Frame )
-//////    {
-//////      char Buffer[ 1024 ];
-//////      Frame->Field( ID3FN_TEXT ).Get( Buffer, sizeof( Buffer ) );
-//////      m_ArtistLabelsStr = wxString( Buffer, wxConvUTF8 );
-//////      m_ArtistLabels = wxStringTokenize( m_ArtistLabelsStr, wxT( "|" ) );
-//////    }
-//////  }
-//////
-//////  if( m_AlbumLabels.Count() == 0 )
-//////  {
-//////    ID3_Frame * Frame = tag->Find( ID3FID_USERTEXT, ID3FN_DESCRIPTION, "guALLABELS" );
-//////    if( Frame )
-//////    {
-//////      char Buffer[ 1024 ];
-//////      Frame->Field( ID3FN_TEXT ).Get( Buffer, sizeof( Buffer ) );
-//////      m_AlbumLabelsStr = wxString( Buffer, wxConvUTF8 );
-//////      m_AlbumLabels = wxStringTokenize( m_AlbumLabelsStr, wxT( "|" ) );
-//////    }
-//////  }
-//////
-  return true;
-}
 
-//// -------------------------------------------------------------------------------- //
-//size_t ID3_GetGenreIndex( char * GenreName )
-//{
-//    size_t index;
-//    if( GenreName && strlen( GenreName ) )
-//    {
-//        for( index = 0; index < ID3_NR_OF_V1_GENRES; index++ )
-//        {
-//            if( !strcasecmp( GenreName, ID3_v1_genre_description[ index ] ) )
-//            {
-//                return index;
-//            }
-//        }
-//    }
-//    return 0xFF;
-//}
-
-//// -------------------------------------------------------------------------------- //
-//ID3_Frame * TagInfo::Find( ID3_Tag * tag, const char * Name )
-//{
-//    ID3_Frame * RetVal = NULL;
-//    ID3_Frame * Frame;
-//    ID3_Tag::Iterator * iter;
-//
-//    iter = tag->CreateIterator();
-//    while( !RetVal && ( Frame = iter->GetNext() ) )
-//    {
-//      if( Frame->GetID() == ID3FID_USERTEXT )
-//      {
-//        char * sDesc = ID3_GetString( Frame, ID3FN_DESCRIPTION );
-//        if( !strcmp( sDesc, Name ) )
-//        {
-//            RetVal = Frame;
-//        }
-//        delete [] sDesc;
-//      }
-//    }
-//    delete iter;
-//
-//    return RetVal;
-//}
-//
-//// -------------------------------------------------------------------------------- //
-//int TagInfo::RemoveLabelFrame( ID3_Tag * tag, const char * Name )
-//{
-//    int RetVal = 0;
-//    ID3_Frame * Frame;
-//    ID3_Tag::Iterator * iter;
-//
-//    iter = tag->CreateIterator();
-//    while( ( Frame = iter->GetNext() ) )
-//    {
-//      if( Frame->GetID() == ID3FID_USERTEXT )
-//      {
-//        //char * sText = ID3_GetString( Frame, ID3FN_TEXT );
-//        char * sDesc = ID3_GetString( Frame, ID3FN_DESCRIPTION );
-//        if( !strcmp( sDesc, Name ) )
-//        {
-//          tag->RemoveFrame( Frame );
-//          RetVal++;
-//        }
-//        //delete [] sText;
-//        delete [] sDesc;
-//      }
-//    }
-//    delete iter;
-//
-//    return RetVal;
-//}
-
-// -------------------------------------------------------------------------------- //
-bool TagInfo::WriteID3Tags( const wxString &FileName )
-{
-//////  ID3_Frame * frame;
-//////
-//////  // Title
-//////  if( !m_TrackName.IsEmpty() )
-//////  {
-//////    ID3_AddTitle( tag, m_TrackName.char_str( wxConvISO8859_1 ), true );
-//////  }
-//////
-//////  // Artist
-//////  if( !m_ArtistName.IsEmpty() )
-//////  {
-//////    ID3_AddArtist( tag, m_ArtistName.char_str( wxConvISO8859_1 ), true );
-//////  }
-//////
-//////  // Album
-//////  if( !m_AlbumName.IsEmpty() )
-//////  {
-//////    ID3_AddAlbum( tag, m_AlbumName.char_str( wxConvISO8859_1 ), true );
-//////  }
-//////
-//////  // Track
-//////  if( m_Track > 0 )
-//////  {
-//////    ID3_RemoveTracks( tag );
-//////    frame = new ID3_Frame( ID3FID_TRACKNUM );
-//////    if( frame )
-//////    {
-//////      frame->GetField( ID3FN_TEXT )->Set( wxString::Format( wxT( "%02u" ), m_Track ).char_str() );
-//////      tag->AttachFrame( frame );
-//////    }
-//////  }
-//////
-//////  // Year
-//////  if( m_Year > 0 )
-//////  {
-//////    ID3_AddYear( tag, wxString::Format( wxT( "%u" ), m_Year ).char_str(), true );
-//////  }
-//////
-//////  // Genre
-//////  if( !m_GenreName.IsEmpty() )
-//////  {
-//////    ID3_RemoveGenres( tag );
-//////    int GenreIndex = ID3_GetGenreIndex( m_GenreName.char_str( wxConvISO8859_1 ) );
-//////    // IDV1 Genres is index to table If valid but not in table must be 0xFF
-//////    frame = new ID3_Frame( ID3FID_CONTENTTYPE );
-//////    if( frame )
-//////    {
-//////      if( GenreIndex == 0xFF )
-//////      {
-//////        frame->GetField( ID3FN_TEXT )->Set( m_GenreName.char_str( wxConvISO8859_1 ) );
-//////      }
-//////      else
-//////      {
-//////        frame->GetField( ID3FN_TEXT )->Set( wxString::Format( wxT( "(%u)" ), GenreIndex ).char_str( wxConvISO8859_1 ) );
-//////      }
-//////      tag->AttachFrame( frame );
-//////    }
-//////  }
-//////
 //////  // The Labels
 //////  if( m_ArtistLabelsStr.IsEmpty() )
 //////  {
@@ -390,3 +329,37 @@ bool TagInfo::WriteID3Tags( const wxString &FileName )
 }
 
 // -------------------------------------------------------------------------------- //
+
+//char* taglib_id3v2_get_user_string(TagLib_File *file,char* description){
+//  ID3v2::Tag* t = get_id3v2_tag(file);
+//
+//  if (t){
+//    ID3v2::UserTextIdentificationFrame* f=ID3v2::UserTextIdentificationFrame::find(t,String(description, unicodeStrings ? String::UTF8 : String::Latin1));
+//    if (f) {
+//      char *s=NULL;
+//      s = ::strdup(f->toString().toCString(unicodeStrings));
+//      if(stringManagementEnabled)
+//        strings.append(s);
+//      return s;
+//    }
+//  }
+//  return 0;
+//}
+//
+//BOOL taglib_id3v2_set_user_string(TagLib_File *file,const char* string,char* description){
+//  ID3v2::Tag* t = get_id3v2_tag(file);
+//
+//  if (t){
+//    ID3v2::UserTextIdentificationFrame* f=ID3v2::UserTextIdentificationFrame::find(t,String(description, unicodeStrings ? String::UTF8 : String::Latin1));
+//    if (f) {
+//      f->setText(String(string, unicodeStrings ? String::UTF8 : String::Latin1));
+//    } else {
+//      f=new ID3v2::UserTextIdentificationFrame(unicodeStrings ? String::UTF8 : String::Latin1);
+//      f->setDescription(String(description, unicodeStrings ? String::UTF8 : String::Latin1));
+//      f->setText(String(string, unicodeStrings ? String::UTF8 : String::Latin1));
+//      t->addFrame(f);
+//    }
+//    return 1;
+//  }
+//  return 0;
+//}
