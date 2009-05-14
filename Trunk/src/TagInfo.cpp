d5889be1f136d8018f5debf10a26797641054971
anonbeat <anonbeat@4cb5adbf-918c-4dcd-961d-2ea90eaf881c>
2009-05-14 08:56:29 +0000

    Finished write id3v2 v2.4 tags

--- Trunk/src/TagInfo.cpp
+++ Trunk/src/TagInfo.cpp
@@ -23,8 +23,8 @@
 
 #include <tag.h>
 #include <fileref.h>
-#include <textidentificationframe.h>
 #include <id3v2framefactory.h>
+#include <textidentificationframe.h>
 #include <id3v2tag.h>
 #include <mpegfile.h>
 
@@ -72,8 +72,9 @@ bool TagInfo::ReadID3Tags( const wxString &FileName )
                 ID3v2::UserTextIdentificationFrame * Frame = ID3v2::UserTextIdentificationFrame::find( tagv2, "guTRLABELS" );
                 if( Frame )
                 {
-                    m_TrackLabelsStr = wxString( Frame->toString().toCString( true ), wxConvUTF8 );
-                    guLogMessage( wxT( "Track Label: '%s'\n" ), m_TrackLabelsStr.c_str() );
+                    // [guTRLABELS] guTRLABELS labels
+                    m_TrackLabelsStr = wxString( Frame->toString().toCString( true ), wxConvUTF8 ).Mid( 24 );
+                    //guLogMessage( wxT( "*Track Label: '%s'\n" ), m_TrackLabelsStr.c_str() );
                     m_TrackLabels = wxStringTokenize( m_TrackLabelsStr, wxT( "|" ) );
                 }
             }
@@ -83,8 +84,8 @@ bool TagInfo::ReadID3Tags( const wxString &FileName )
                 ID3v2::UserTextIdentificationFrame * Frame = ID3v2::UserTextIdentificationFrame::find( tagv2, "guARLABELS" );
                 if( Frame )
                 {
-                    m_ArtistLabelsStr = wxString( Frame->toString().toCString( true ), wxConvUTF8 );
-                    guLogMessage( wxT( "Artist Label: '%s'\n" ), m_ArtistLabelsStr.c_str() );
+                    m_ArtistLabelsStr = wxString( Frame->toString().toCString( true ), wxConvUTF8 ).Mid( 24 );
+                    //guLogMessage( wxT( "*Artist Label: '%s'\n" ), m_ArtistLabelsStr.c_str() );
                     m_ArtistLabels = wxStringTokenize( m_TrackLabelsStr, wxT( "|" ) );
                 }
             }
@@ -94,8 +95,8 @@ bool TagInfo::ReadID3Tags( const wxString &FileName )
                 ID3v2::UserTextIdentificationFrame * Frame = ID3v2::UserTextIdentificationFrame::find( tagv2, "guALLABELS" );
                 if( Frame )
                 {
-                    m_AlbumLabelsStr = wxString( Frame->toString().toCString( true ), wxConvUTF8 );
-                    guLogMessage( wxT( "Album Label: '%s'\n" ), m_AlbumLabelsStr.c_str() );
+                    m_AlbumLabelsStr = wxString( Frame->toString().toCString( true ), wxConvUTF8 ).Mid( 24 );
+                    //guLogMessage( wxT( "*Album Label: '%s'\n" ), m_AlbumLabelsStr.c_str() );
                     m_AlbumLabels = wxStringTokenize( m_TrackLabelsStr, wxT( "|" ) );
                 }
             }
@@ -114,19 +115,22 @@ bool TagInfo::ReadID3Tags( const wxString &FileName )
 void ID3v2_CheckLabelFrame( ID3v2::Tag * tagv2, const char * description, const wxString &value )
 {
     ID3v2::UserTextIdentificationFrame * frame;
+
+    //guLogMessage( wxT( "USERTEXT[ %s ] = '%s'" ), wxString( description, wxConvISO8859_1 ).c_str(), value.c_str() );
+
     frame = ID3v2::UserTextIdentificationFrame::find( tagv2, description );
     if( frame )
     {
-        if( value.IsEmpty() )
-        {
-            while( frame )
-            {
-                // remove the frame
-                tagv2->removeFrame( frame );
-                frame = ID3v2::UserTextIdentificationFrame::find( tagv2, description );
-            }
-        }
-        else
+//        if( value.IsEmpty() )
+//        {
+//            while( frame )
+//            {
+//                // remove the frame
+//                tagv2->removeFrame( frame );
+//                frame = ID3v2::UserTextIdentificationFrame::find( tagv2, description );
+//            }
+//        }
+//        else
         {
             frame->setText( ( char * ) value.char_str( wxConvISO8859_1 ) );
         }
@@ -137,7 +141,7 @@ void ID3v2_CheckLabelFrame( ID3v2::Tag * tagv2, const char * description, const 
         {
             frame = new ID3v2::UserTextIdentificationFrame( TagLib::String::UTF8 );
             frame->setDescription( TagLib::String( description, TagLib::String::UTF8 ) );
-            frame->setText( ( char * ) value.char_str( wxConvUTF8 ) );
+            frame->setText( ( char * ) value.char_str( wxConvISO8859_1 ) );
             tagv2->addFrame( frame );
         }
     }
@@ -157,7 +161,7 @@ bool TagInfo::WriteID3Tags( const wxString &FileName )
             tag->setArtist( ( char * ) m_ArtistName.char_str( wxConvISO8859_1 ) );
             tag->setAlbum( ( char * ) m_AlbumName.char_str( wxConvISO8859_1 ) );
             tag->setGenre( ( char * ) m_GenreName.char_str( wxConvISO8859_1 ) );
-            tag->setTrack( m_Track );
+            tag->setTrack( m_Track ); // set the id3v1 track
             tag->setYear( m_Year );
         }
 
@@ -165,7 +169,9 @@ bool TagInfo::WriteID3Tags( const wxString &FileName )
         ID3v2::Tag * tagv2 = ( ( TagLib::MPEG::File * ) fileref.file() )->ID3v2Tag();
         if( tagv2 )
         {
-
+            // I have found several TRCK fields in the mp3s
+            tagv2->removeFrames( "TRCK" );
+            tagv2->setTrack( m_Track );
             // The Labels
             if( m_ArtistLabelsStr.IsEmpty() )
             {
@@ -230,136 +236,7 @@ bool TagInfo::WriteID3Tags( const wxString &FileName )
     }
 
 
-
-//////  // The Labels
-//////  if( m_ArtistLabelsStr.IsEmpty() )
-//////  {
-//////    if( m_ArtistLabels.Count() )
-//////    {
-//////      int index;
-//////      int count = m_ArtistLabels.Count();
-//////      for( index = 0; index < count; index++ )
-//////      {
-//////        m_ArtistLabelsStr += m_ArtistLabels[ index ];
-//////        m_ArtistLabelsStr += wxT( "|" );
-//////      }
-//////      m_ArtistLabelsStr.RemoveLast( 1 );
-//////    }
-//////  }
-//////
-//////
-////////  if( !ArtistLabelsStr.IsEmpty() )
-//////  {
-//////    frame = Find( tag, "guARLABELS" );
-//////    if( !frame )
-//////    {
-//////      frame = new ID3_Frame( ID3FID_USERTEXT );
-//////      frame->Field( ID3FN_TEXTENC ) = ID3TE_ASCII;
-//////      frame->Field( ID3FN_DESCRIPTION ) = "guARLABELS";
-//////      tag->AttachFrame( frame );
-//////    }
-//////    frame->Field( ID3FN_TEXT ) = m_ArtistLabelsStr.char_str();
-//////  }
-//////
-//////
-//////  if( m_AlbumLabelsStr.IsEmpty() )
-//////  {
-//////    if( m_AlbumLabels.Count() )
-//////    {
-//////      int index;
-//////      int count = m_AlbumLabels.Count();
-//////      for( index = 0; index < count; index++ )
-//////      {
-//////        m_AlbumLabelsStr += m_AlbumLabels[ index ];
-//////        m_AlbumLabelsStr += wxT( "|" );
-//////      }
-//////      m_AlbumLabelsStr.RemoveLast( 1 );
-//////    }
-//////  }
-//////
-//////
-////////  if( !AlbumLabelsStr.IsEmpty() )
-//////  {
-//////    frame = Find( tag, "guALLABELS" );
-//////    if( !frame )
-//////    {
-//////      //guLogMessage( wxT( "Creating guALLABELS Frame..." ) );
-//////      frame = new ID3_Frame( ID3FID_USERTEXT );
-//////      frame->Field( ID3FN_TEXTENC ) = ID3TE_ASCII;
-//////      frame->Field( ID3FN_DESCRIPTION ) = "guALLABELS";
-//////      tag->AttachFrame( frame );
-//////    }
-//////    frame->Field( ID3FN_TEXT ) = m_AlbumLabelsStr.char_str();
-//////  }
-//////
-//////
-//////  if( m_TrackLabelsStr.IsEmpty() )
-//////  {
-//////    if( m_TrackLabels.Count() )
-//////    {
-//////      int index;
-//////      int count = m_TrackLabels.Count();
-//////      for( index = 0; index < count; index++ )
-//////      {
-//////        m_TrackLabelsStr += m_TrackLabels[ index ];
-//////        m_TrackLabelsStr += wxT( "|" );
-//////      }
-//////      m_TrackLabelsStr.RemoveLast( 1 );
-//////    }
-//////  }
-//////
-//////
-////////  if( !TrackLabelsStr.IsEmpty() )
-//////  {
-//////    frame = Find( tag, "guTRLABELS" );
-//////    if( !frame )
-//////    {
-//////      frame = new ID3_Frame( ID3FID_USERTEXT );
-//////      frame->Field( ID3FN_TEXTENC ) = ID3TE_ASCII;
-//////      frame->Field( ID3FN_DESCRIPTION ) = "guTRLABELS";
-//////      tag->AttachFrame( frame );
-//////    }
-//////    frame->Field( ID3FN_TEXT ) = m_TrackLabelsStr.char_str();
-//////  }
-//////
-//////  tag->SetPadding( false );
-//////  tag->Update();
-//////
   return true;
 }
 
 // -------------------------------------------------------------------------------- //
-
-//char* taglib_id3v2_get_user_string(TagLib_File *file,char* description){
-//  ID3v2::Tag* t = get_id3v2_tag(file);
-//
-//  if (t){
-//    ID3v2::UserTextIdentificationFrame* f=ID3v2::UserTextIdentificationFrame::find(t,String(description, unicodeStrings ? String::UTF8 : String::Latin1));
-//    if (f) {
-//      char *s=NULL;
-//      s = ::strdup(f->toString().toCString(unicodeStrings));
-//      if(stringManagementEnabled)
-//        strings.append(s);
-//      return s;
-//    }
-//  }
-//  return 0;
-//}
-//
-//BOOL taglib_id3v2_set_user_string(TagLib_File *file,const char* string,char* description){
-//  ID3v2::Tag* t = get_id3v2_tag(file);
-//
-//  if (t){
-//    ID3v2::UserTextIdentificationFrame* f=ID3v2::UserTextIdentificationFrame::find(t,String(description, unicodeStrings ? String::UTF8 : String::Latin1));
-//    if (f) {
-//      f->setText(String(string, unicodeStrings ? String::UTF8 : String::Latin1));
-//    } else {
-//      f=new ID3v2::UserTextIdentificationFrame(unicodeStrings ? String::UTF8 : String::Latin1);
-//      f->setDescription(String(description, unicodeStrings ? String::UTF8 : String::Latin1));
-//      f->setText(String(string, unicodeStrings ? String::UTF8 : String::Latin1));
-//      t->addFrame(f);
-//    }
-//    return 1;
-//  }
-//  return 0;
-//}
