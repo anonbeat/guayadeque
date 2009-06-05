aa47d3400685e39ac464dcff64ed75f68366a1e7
anonbeat <anonbeat@4cb5adbf-918c-4dcd-961d-2ea90eaf881c>
2009-06-05 10:07:30 +0000

    Added the OnlineLinks files missing in previous patch

---
+++ Trunk/src/OnlineLinks.cpp
@@ -0,0 +1,72 @@
+// -------------------------------------------------------------------------------- //
+//	Copyright (C) 2008-2009 J.Rios
+//	anonbeat@gmail.com
+//
+//    This Program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2, or (at your option)
+//    any later version.
+//
+//    This Program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; see the file LICENSE.  If not, write to
+//    the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+//    http://www.gnu.org/copyleft/gpl.html
+//
+// -------------------------------------------------------------------------------- //
+#include "OnlineLinks.h"
+
+#include "Commands.h"
+#include "Config.h"
+#include "Images.h"
+
+#include <wx/uri.h>
+
+
+// -------------------------------------------------------------------------------- //
+void AddOnlineLinksMenu( wxMenu * Menu )
+{
+    wxMenu * SubMenu;
+    int index;
+    int count;
+    wxMenuItem * MenuItem;
+    if( Menu )
+    {
+        SubMenu = new wxMenu();
+        wxASSERT( SubMenu );
+
+        guConfig * Config = ( guConfig * ) guConfig::Get();
+        wxArrayString Links = Config->ReadAStr( wxT( "Link" ), wxEmptyString, wxT( "SearchLinks" ) );
+        if( ( count = Links.Count() ) )
+        {
+            for( index = 0; index < count; index++ )
+            {
+                wxURI Uri( Links[ index ] );
+                MenuItem = new wxMenuItem( Menu, ID_LASTFM_SEARCH_LINK + index, Uri.GetServer(), Links[ index ] );
+                wxString IconFile = wxGetHomeDir() + wxT( "/.guayadeque/LinkIcons/" ) + Uri.GetServer() + wxT( ".ico" );
+                if( wxFileExists( IconFile ) )
+                {
+                    MenuItem->SetBitmap( wxBitmap( IconFile, wxBITMAP_TYPE_ICO ) );
+                }
+                else
+                {
+                    MenuItem->SetBitmap( wxBitmap( guImage_search ) );
+                }
+                SubMenu->Append( MenuItem );
+            }
+        }
+        else
+        {
+            MenuItem = new wxMenuItem( Menu, -1, _( "No search link defined" ), _( "Add search links in preferences" ) );
+            MenuItem->SetBitmap( wxBitmap( guImage_search ) );
+            SubMenu->Append( MenuItem );
+        }
+        Menu->AppendSubMenu( SubMenu, _( "Links" ) );
+    }
+}
+
+// -------------------------------------------------------------------------------- //
