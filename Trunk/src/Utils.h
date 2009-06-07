f5257a2d962aa0bb62b6889c4b45a42417cdd2a6
anonbeat <anonbeat@4cb5adbf-918c-4dcd-961d-2ea90eaf881c>
2009-06-07 14:27:41 +0000

    Fixed bug dragging items in playlist when the list have been scrolled

--- Trunk/src/Utils.h
+++ Trunk/src/Utils.h
@@ -35,10 +35,6 @@
 #define guRandomInit() (srand( time( NULL ) ))
 #define guRandom(x) (rand() % x)
 
-#define guMin(X,Y) ((X) < (Y) ? (X) : (Y))
-#define guMax(X,Y) ((X) > (Y) ? (X) : (Y))
-
-
 // -------------------------------------------------------------------------------- //
 wxString LenToString( int Len );
 wxArrayString guSplitWords( const wxString &InputStr );
