438b04cd574489a0df84dd9cb063830a652dad5a
anonbeat <anonbeat@4cb5adbf-918c-4dcd-961d-2ea90eaf881c>
2010-01-07 14:32:34 +0000

    Added Silence Detector in preferences

--- src/Images.cpp
+++ src/Images.cpp
@@ -106,6 +106,7 @@
 #include "./images/pref_library.h"
 #include "./images/pref_links.h"
 #include "./images/pref_online_services.h"
+#include "./images/pref_playback.h"
 #include "./images/pref_podcasts.h"
 
 // -------------------------------------------------------------------------------- //
@@ -202,6 +203,7 @@ guImage_Item guImage_Items[] = {
     GUIMAGE( guImage_pref_library,          wxBITMAP_TYPE_PNG ),
     GUIMAGE( guImage_pref_links,            wxBITMAP_TYPE_PNG ),
     GUIMAGE( guImage_pref_online_services,  wxBITMAP_TYPE_PNG ),
+    GUIMAGE( guImage_pref_playback,         wxBITMAP_TYPE_PNG ),
     GUIMAGE( guImage_pref_podcasts,         wxBITMAP_TYPE_JPEG )
 };
 
