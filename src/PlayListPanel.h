5c966e709ea76510668b32aae650e3133c6456c6
anonbeat <anonbeat@4cb5adbf-918c-4dcd-961d-2ea90eaf881c>
2010-05-25 09:20:14 +0000

    Implementing enqueue as next tracks 3

--- src/PlayListPanel.h
+++ src/PlayListPanel.h
@@ -140,6 +140,7 @@ class guPlayListPanel : public wxPanel
     void                OnPLNamesActivated( wxTreeEvent &event );
     void                OnPLNamesPlay( wxCommandEvent &event );
     void                OnPLNamesEnqueue( wxCommandEvent &event );
+    void                OnPLNamesEnqueueAsNext( wxCommandEvent &event );
     void                OnPLNamesNewPlaylist( wxCommandEvent &event );
     void                OnPLNamesEditPlaylist( wxCommandEvent &event );
     void                OnPLNamesRenamePlaylist( wxCommandEvent &event );
@@ -154,7 +155,9 @@ class guPlayListPanel : public wxPanel
     void                OnPLTracksPlayClicked( wxCommandEvent &event );
     void                OnPLTracksPlayAllClicked( wxCommandEvent &event );
     void                OnPLTracksQueueClicked( wxCommandEvent &event );
+    void                OnPLTracksQueueAsNextClicked( wxCommandEvent &event );
     void                OnPLTracksQueueAllClicked( wxCommandEvent &event );
+    void                OnPLTracksQueueAllAsNextClicked( wxCommandEvent &event );
     void                OnPLTracksEditLabelsClicked( wxCommandEvent &event );
     void                OnPLTracksEditTracksClicked( wxCommandEvent &event );
     void                OnPLTracksCopyToClicked( wxCommandEvent &event );
