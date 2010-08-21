1a54f169522a47f86628c72c52ceed95a92c812c
anonbeat <anonbeat@4cb5adbf-918c-4dcd-961d-2ea90eaf881c>
2010-08-21 15:34:13 +0000

    Implemented Edit fields from tracks list box

---
+++ src/FieldEditor.cpp
@@ -0,0 +1,73 @@
+// -------------------------------------------------------------------------------- //
+//	Copyright (C) 2008-2010 J.Rios
+//	anonbeat@gmail.com
+//
+//    This Program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2, or (at your option)
+//    any later version.
+//
+//    This Program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; see the file LICENSE.  If not, write to
+//    the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+//    http://www.gnu.org/copyleft/gpl.html
+//
+// -------------------------------------------------------------------------------- //
+#include "FieldEditor.h"
+
+// -------------------------------------------------------------------------------- //
+guFieldEditor::guFieldEditor( wxWindow * parent, const wxString &label, const wxString &defval, const wxArrayString &items ) :
+    wxDialog( parent, wxID_ANY, _( "Field Editor" ), wxDefaultPosition, wxSize( 370, 175 ), wxDEFAULT_DIALOG_STYLE )
+{
+	SetSizeHints( wxDefaultSize, wxDefaultSize );
+
+	wxBoxSizer * MainFrame;
+	MainFrame = new wxBoxSizer( wxVERTICAL );
+
+	wxBoxSizer * TopFrame;
+	TopFrame = new wxBoxSizer( wxHORIZONTAL );
+
+	wxStaticText * EditLabel = new wxStaticText( this, wxID_ANY, label + wxT( " :" ), wxDefaultPosition, wxDefaultSize, 0 );
+	EditLabel->Wrap( -1 );
+	TopFrame->Add( EditLabel, 0, wxALL|wxALIGN_CENTER_VERTICAL, 5 );
+
+	m_EditComboBox = new wxComboBox( this, wxID_ANY, defval, wxDefaultPosition, wxDefaultSize, 0, NULL, 0 );
+    m_EditComboBox->Append( items );
+	TopFrame->Add( m_EditComboBox, 1, wxTOP|wxBOTTOM|wxRIGHT|wxALIGN_CENTER_VERTICAL, 5 );
+
+	MainFrame->Add( TopFrame, 1, wxEXPAND, 5 );
+
+	wxStdDialogButtonSizer * ButtonFrame = new wxStdDialogButtonSizer();
+	wxButton * ButtonOK = new wxButton( this, wxID_OK );
+	ButtonFrame->AddButton( ButtonOK );
+	wxButton * ButtonCancel = new wxButton( this, wxID_CANCEL );
+	ButtonFrame->AddButton( ButtonCancel );
+	ButtonFrame->Realize();
+	MainFrame->Add( ButtonFrame, 0, wxEXPAND|wxTOP|wxBOTTOM|wxRIGHT, 5 );
+
+	SetSizer( MainFrame );
+	Layout();
+
+}
+
+// -------------------------------------------------------------------------------- //
+guFieldEditor::~guFieldEditor()
+{
+}
+
+// -------------------------------------------------------------------------------- //
+wxString guFieldEditor::GetData( void )
+{
+    int Selected = m_EditComboBox->GetCurrentSelection();
+    if( Selected != wxNOT_FOUND )
+        return m_EditComboBox->GetString( Selected );
+
+    return m_EditComboBox->GetValue();
+}
+
+// -------------------------------------------------------------------------------- //
