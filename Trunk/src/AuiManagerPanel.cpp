661982c5f50d80a6681fb35ba9d977d996ce13f9
anonbeat <anonbeat@4cb5adbf-918c-4dcd-961d-2ea90eaf881c>
2012-10-13 06:49:54 +0000

    Fix creating child items order in tunein radios

--- Trunk/src/AuiManagerPanel.cpp
+++ Trunk/src/AuiManagerPanel.cpp
@@ -55,21 +55,21 @@ guAuiManagerPanel::guAuiManagerPanel( wxWindow * parent ) :
     AuiDockArt->SetColour( wxAUI_DOCKART_ACTIVE_CAPTION_COLOUR, BaseColor );
 
     AuiDockArt->SetColour( wxAUI_DOCKART_ACTIVE_CAPTION_GRADIENT_COLOUR,
-            wxAuiStepColour( BaseColor, 80 ) );
+            wxAuiStepColour( BaseColor, 93 ) );
 
     AuiDockArt->SetColour( wxAUI_DOCKART_INACTIVE_CAPTION_COLOUR,
                           BaseColor );
 
     AuiDockArt->SetColour( wxAUI_DOCKART_INACTIVE_CAPTION_GRADIENT_COLOUR,
-            wxAuiStepColour( BaseColor, 80 ) );
+            wxAuiStepColour( BaseColor, 93 ) );
 
-    AuiDockArt->SetColour( wxAUI_DOCKART_SASH_COLOUR, wxAuiStepColour( BaseColor, 90 ) );
+    AuiDockArt->SetColour( wxAUI_DOCKART_SASH_COLOUR, wxAuiStepColour( BaseColor, 98 ) );
 
     AuiDockArt->SetMetric( wxAUI_DOCKART_PANE_BORDER_SIZE, 0 );
     //AuiDockArt->SetMetric( wxAUI_DOCKART_PANE_BUTTON_SIZE, 16 );
     AuiDockArt->SetMetric( wxAUI_DOCKART_CAPTION_SIZE, 22 );
     AuiDockArt->SetMetric( wxAUI_DOCKART_PANE_BORDER_SIZE, 1 );
-    AuiDockArt->SetMetric( wxAUI_DOCKART_SASH_SIZE, 3 );
+    AuiDockArt->SetMetric( wxAUI_DOCKART_SASH_SIZE, 5 );
 
     AuiDockArt->SetMetric( wxAUI_DOCKART_GRADIENT_TYPE, wxAUI_GRADIENT_VERTICAL );
 
