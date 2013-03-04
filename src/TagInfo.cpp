01cc0ccef8514a0478e74f22ce3dd39622fb30cd
anonbeat <anonbeat@4cb5adbf-918c-4dcd-961d-2ea90eaf881c>
2013-03-04 11:31:31 +0000

    Change license to ApeTag and TagInfo

--- src/TagInfo.cpp
+++ src/TagInfo.cpp
@@ -2,11 +2,10 @@
 //	Copyright (C) 2008-2013 J.Rios
 //	anonbeat@gmail.com
 //
-//    This Program is free software; you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation; either version 2, or (at your option)
-//    any later version.
-//    
+//    This library is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Lesser General Public
+//    License as published by the Free Software Foundation; either
+//    version 2.1 of the License, or (at your option) any later version.
 //
 //    This Program is distributed in the hope that it will be useful,
 //    but WITHOUT ANY WARRANTY; without even the implied warranty of
