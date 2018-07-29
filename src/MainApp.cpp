5a6b44ab3f01e522f0e1704213455401964ff0a6
J.Rios <anonbeat@gmail.com>
2018-07-29 10:21:04 +0100

    Update MainApp.cpp

--- src/MainApp.cpp
+++ src/MainApp.cpp
@@ -47,7 +47,7 @@ guMainApp::guMainApp() : wxApp()
 {
 //    m_Db = NULL;
     m_DbCache = NULL;
-    m_SingleInstanceChecker = nullptr;
+    m_SingleInstanceChecker = NULL;
 
 #if wxUSE_ON_FATAL_EXCEPTION    // Thanks TheBigRed
         wxHandleFatalExceptions();
